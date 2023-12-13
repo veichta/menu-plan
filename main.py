6ff3b6bd3f9358cfded6b0310335ccbcf9097311
sscholbe <sscholbe@ethz.ch>
2023-12-13 18:23:14 +0100

    Added printing of weather

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ import numpy as np
 from src.constants import MensaNames, MensaURL
 from src.scraping import get_menus
 from src.utils import print_menu
+from src.weather import get_weather
 
 
 def main(args):
     menu_df = get_menus()
 
-    print_welcome_message()
+    print_welcome_message(args)
 
     menu_per_mensa = menu_df.groupby("mensa")
 
@@ -37,18 +38,23 @@ def main(args):
         idx += 1
 
 
-def print_welcome_message():
+def print_welcome_message(args):
     # with open("data/messages.json", "r") as f:
     #     messages = json.load(f)
     # greeting_msg = np.random.choice(messages["welcome-messages"])
     # print(f"{greeting_msg} Today is {MensaURL().weekday} and here is today's menu:\n")
 
     if MensaURL().weekday == "dienstag":
-        print(f"Today is vinstag (so don't foget your stamp cards) and here is today's menu:\n")
+        print(f"Today is vinstag (so don't foget your stamp cards) and here is today's menu:")
     elif MensaURL().weekday == "mittwoch":
-        print(f"Today is mojitwoch and here is today's menu:\n")
+        print(f"Today is mojitwoch and here is today's menu:")
     else:
-        print(f"Today is {MensaURL().weekday} and here is today's menu:\n")
+        print(f"Today is {MensaURL().weekday} and here is today's menu:")
+
+    # Get the weather at Fluntern (8044)
+    hour = 12 if args.time == "mittag" else 18
+    temperature, emoji = get_weather(8044, hour)
+    print(f"The weather at {hour}:00 is {emoji} and {int(temperature)} °C.\n")
 
 
 if __name__ == "__main__":
